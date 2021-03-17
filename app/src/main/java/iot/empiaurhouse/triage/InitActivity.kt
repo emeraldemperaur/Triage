b0896ce6fbd6b4d3f5c04ffeae8603e8d5490f3b
Emeka Egwim <egwim.emeka@gmail.com>
2021-03-17 00:53:58 -0600

    Implementing init UI View Handlers, Intents and Transition(s)

--- app/src/main/java/iot/empiaurhouse/triage/InitActivity.kt
+++ app/src/main/java/iot/empiaurhouse/triage/InitActivity.kt
@@ -1,17 +1,67 @@
 package iot.empiaurhouse.triage
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import iot.empiaurhouse.triage.databinding.ActivityInitBinding
 
 class InitActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityInitBinding
+    private lateinit var fadeInAnimation : Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInitBinding.inflate(layoutInflater)
         val viewInit = binding.root
+        fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fader)
         setContentView(viewInit)
+        binding.title.startAnimation(fadeInAnimation)
+        fadeInAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.subtitle.visibility = View.VISIBLE
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@InitActivity, SetupActivity::class.java))
+                    finish()
+                }, 3000)
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+            }
+        })
+
+
+
     }
+
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
+
+
 }
--- app/src/main/java/iot/empiaurhouse/triage/LaunchActivity.kt
+++ app/src/main/java/iot/empiaurhouse/triage/LaunchActivity.kt
@@ -1,17 +1,69 @@
 package iot.empiaurhouse.triage
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import iot.empiaurhouse.triage.databinding.ActivityLaunchBinding
 
 class LaunchActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLaunchBinding
+    private lateinit var fadeInAnimation : Animation
+    private lateinit var rotationAnimation : Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLaunchBinding.inflate(layoutInflater)
         val viewLaunch = binding.root
+        fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        rotationAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate)
         setContentView(viewLaunch)
+        binding.devIntroLogo.visibility = View.INVISIBLE
+        binding.devIntroLogo.startAnimation(fadeInAnimation)
+        fadeInAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.devIntroLogo.startAnimation(rotationAnimation)
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@LaunchActivity, InitActivity::class.java))
+                    finish()
+                }, 2696)
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+                binding.devIntroLogo.visibility = View.VISIBLE
+            }
+        })
+
+
     }
+
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
+
 }
