078955a2bb6289a4e11d1fe55b0a71b1eead856e
Emeka Egwim <egwim.emeka@gmail.com>
2021-11-13 14:35:08 -0700

    Implementing InsightModelDetail UI w/ InsightModelController.kt

--- app/src/main/java/iot/empiaurhouse/triage/view/SettingsFragment.kt
+++ app/src/main/java/iot/empiaurhouse/triage/view/SettingsFragment.kt
@@ -1,11 +1,11 @@
 package iot.empiaurhouse.triage.view
 
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.preference.PreferenceFragmentCompat
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import iot.empiaurhouse.triage.R
@@ -16,13 +16,15 @@ private const val ARG_PARAM1 = ""
 private const val ARG_PARAM2 = ""
 
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var binding: FragmentSettingsBinding
     private lateinit var hubUserName: TextView
     private lateinit var searchButton: FloatingActionButton
     private lateinit var toolbarView: CollapsingToolbarLayout
+    private var themedContext: Context? = null
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,24 +35,56 @@ class SettingsFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentSettingsBinding.bind(view)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.triage_preferences, rootKey)
         hubUserName = requireActivity().findViewById(R.id.hub_username_title)
         searchButton = requireActivity().findViewById(R.id.hub_search_button)
         toolbarView = requireActivity().findViewById(R.id.hub_collapsing_toolbar)
         hubUserName.visibility = View.GONE
         searchButton.visibility = View.GONE
         toolbarView.visibility = View.GONE
+
+
+    }
+
+    /*override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_settings, container, false)
+        container!!.context.setTheme(R.style.PreferencesTheme);
+
+        return null
+    }*/
+
+    override fun onResume() {
+        super.onResume()
+        hubUserName.visibility = View.GONE
+        searchButton.visibility = View.GONE
+        toolbarView.visibility = View.GONE
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context).also {
+            themedContext = ContextThemeWrapper(context, R.style.PreferencesTheme)
+            // if you want to apply a theme overlay:
+            // themedContext.theme.applyStyle(R.style.MyThemeOverlay, true)
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        themedContext = null
+    }
+
+    override fun getContext(): Context? {
+        return themedContext ?: super.getContext()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //binding = FragmentSettingsBinding.bind(view)
 
     }
 
