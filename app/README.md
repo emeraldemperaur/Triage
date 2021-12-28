bdd2b31a0a09c9755d66471071346b8a67067b12
Emeka Egwim <egwim.emeka@gmail.com>
2021-12-28 00:55:16 -0700

    Implementing InsightModel data render w/ InsightEngine.kt

---
+++ app/README.md
@@ -0,0 +1,34 @@
+# Triage /trēˈäZH/
+## Chiron Medical Practice Management System - Mobile Data Pivot & Analysis Tool (Android MVVM)
+
+### Overview
+<p align="justify">Triage is a data pivot and analysis tool designed as a complimentary mobile application to sort, filter, pivot and evaluate data sourced from a Chiron web app instance via a RESTful API. The application architecture design is based on the MVVM approach Built using Kotlin, SQLite, Retrofit, Material Design, Android Jetpack Architecture Components and Firebase Real Time Database & Machine Learning.</p>
+
+## Related Links
+
+**Portfolio URL >_** https://www.mekaegwim.ca/portfolio/
+<br>
+**Chiron Demo URL >_** https://chiron-cloudapp.herokuapp.com/
+<br>
+**Chiron Git >_** https://github.com/emeraldemperaur/chiron
+
+## System Requirements
+
+```
+> Android 10 Operating System or Higher
+> Internet Connection
+
+```
+
+<br><br>
+### UI/UX Screenshots
+&nbsp;
+<h4>Login</h4>
+<br>
+<h4>Dashboard</h4>
+<br>
+<h4>Insight Models</h4>
+<br>
+<h4>Data Pivots</h4>
+<br>
+<h4>Preferences</h4>
--- app/src/main/java/iot/empiaurhouse/triage/utils/InsightEngine.kt
+++ app/src/main/java/iot/empiaurhouse/triage/utils/InsightEngine.kt
@@ -6,19 +6,42 @@ import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.charts.ScatterChart
-import iot.empiaurhouse.triage.model.InsightModel
+import iot.empiaurhouse.triage.model.*
 
 class InsightEngine {
+
+    private lateinit var patientsRecord: ArrayList<Patient>
+    private lateinit var diagnosesRecord: ArrayList<Diagnosis>
+    private lateinit var prescriptionsRecord: ArrayList<Prescription>
+    private lateinit var visitsRecord: ArrayList<Visit>
+    private lateinit var pharmaceuticalRecord: ArrayList<Pharmaceuticals>
+
 
     fun initInsightRender(insightModel: InsightModel, insightRenderView: ConstraintLayout,
                           juxtapositionVistaView: ConstraintLayout, histogramVista: BarChart,
                           pieChartVista: PieChart, lineChartVista: LineChart, scatterPlotVista: ScatterChart,
                           histogramJuxVista: BarChart, pieChartJuxVista: PieChart, lineChartJuxVista: LineChart,
-                          scatterPlotJuxVista: ScatterChart, viewDivider: View){
+                          scatterPlotJuxVista: ScatterChart, viewDivider: View,
+                          patientRecords: ArrayList<Patient>? = null, diagnosisRecords: ArrayList<Diagnosis>? = null,
+                          prescriptionRecords: ArrayList<Prescription>? = null, visitRecords: ArrayList<Visit>? = null,
+                          pharmaceuticalsRecords: ArrayList<Pharmaceuticals>? = null){
 
         insightRenderView.visibility = View.VISIBLE
-
-
+        if (!patientRecords.isNullOrEmpty()){
+            patientsRecord = patientRecords
+        }
+        if (!diagnosisRecords.isNullOrEmpty()){
+            diagnosesRecord = diagnosisRecords
+        }
+        if (!prescriptionRecords.isNullOrEmpty()){
+            prescriptionsRecord = prescriptionRecords
+        }
+        if (!visitRecords.isNullOrEmpty()){
+            visitsRecord = visitRecords
+        }
+        if (!pharmaceuticalsRecords.isNullOrEmpty()){
+            pharmaceuticalRecord = pharmaceuticalsRecords
+        }
         when(insightModel.vistaCode){
             1 ->{
                 histogramVista.visibility = View.VISIBLE
